f9d5b179325da9f0149339ef04c8af53a9897def
Gonzalo Beltran <gobemo@gmail.com>
2022-05-12 11:06:14 +0200

    Exercise 4

---
+++ workshop_01/Exercise-4.py
@@ -0,0 +1,21 @@
+## This is exercise 3 of the Python course done at open.bootcamp.com
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+a = "Hola Mundo!"
+print (a)
+print(type (a))
+
+a = "Change the attribute a"
+
+print (a)
+print(type (a))
+
+b = 10
+print (b)
+print(type(b))
+
+b = 20
+
+print (b)
+print(type(b))
