bf68b1fa34babad200cc8bec790b1fc9b639fd32
Gonzalo Beltran <gobemo@gmail.com>
2022-05-12 11:00:49 +0200

    Exercise 3

---
+++ workshop_01/Exercise-3.py
@@ -0,0 +1,11 @@
+## This is exercise 3 of the Python course done at open.bootcamp.com
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+a = "Hola Mundo!"
+print (a)
+print(type (a))
+
+b = 10
+print (b)
+print(type(b))
