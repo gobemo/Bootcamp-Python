52a9825eeae395b7a351240b4e6f139cd1888118
Gonzalo Beltran <gobemo@gmail.com>
2022-05-12 09:33:46 +0200

    Create Exercise-1.py

---
+++ Exercise-1.py
@@ -0,0 +1,7 @@
+## This is exercise 1 of the Python course done at open.bootcamp.com
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+print("Hola , soy Gonzalo Beltran")
+print ("Estoy empezando el curso de python")
+print("Espero aprender mucho")
