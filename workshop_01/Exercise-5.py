4d264c5412bd4ef4a940a8575889f8a147706c63
Gonzalo Beltran <gobemo@gmail.com>
2022-05-14 12:10:33 +0200

    Exercise 5

---
+++ workshop_01/Exercise-5.py
@@ -0,0 +1,28 @@
+## This is exercise 5 of the Python course done at open.bootcamp.com
+## Create variables
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+from pickle import FALSE, TRUE
+from tokenize import Name
+
+
+Name = "Carlos"
+Last_Name = "Bermudez"
+Age = 40
+Email = "cbermudez@gmail.com"
+Telephone = "310345763"
+Married = True
+Children = False
+List_Friends = ["Juan", "Manuel","Jorge","Ernesto"]
+Films_viewed = {"Peli_01":"Batman","Peli_02":"Spiderman","Peli_03":"Loney Tunes","Peli_04":"Netflix","Peli_05":"Harrie Potter"}
+
+print ("Name: " + Name)
+print ("Last_Name: " + Last_Name)
+print ("Age: ", Age)
+print ("Email: " + Email)
+print ("Telephone: " + Telephone)
+print ("Married: ", Married)
+print ("Children: " + str(Children))
+print ("List ∫Friends: ", List_Friends)
+print ("Films_ iewed: ", Films_viewed)
