f127a3f6c50587cb1522a119251d89bf451f0d06
Gonzalo Beltran <gobemo@gmail.com>
2022-05-19 10:12:29 +0200

    Session 4 Exercise 3

---
+++ Session 4/Session4-Exercise-3.py
@@ -0,0 +1,12 @@
+## This is exercise 1 session 4 of the Python course done at open.bootcamp.com
+## Printing odd numbers
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+List = []
+i = 1
+for k in range(100):
+    List.append(i)
+    i += 1
+
+print (sorted(List, reverse = True))
