a6a4bb8ce42dab529fa30463bb0cd7937ae1786c
Gonzalo Beltran <gobemo@gmail.com>
2022-05-19 09:31:54 +0200

    Session 4 Exercise 1

---
+++ Session 4/Session4-Exercise-1.py
@@ -0,0 +1,11 @@
+## This is exercise 1 session 4 of the Python course done at open.bootcamp.com
+## Validation of legal age
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+edad = int (input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
