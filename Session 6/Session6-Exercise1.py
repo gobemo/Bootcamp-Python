56f7c78b0ed6ed0d4346239a731f3ff0a57edb80
Gonzalo Beltran <gobemo@gmail.com>
2022-05-21 11:36:50 +0200

    Session6 Exercise 1

---
+++ Session 6/Session6-Exercise1.py
@@ -0,0 +1,20 @@
+## This is exercise 1 session 5 of the Python course done at open.bootcamp.com
+## Define class cat
+## Gonzalo Beltran Morera
+## gobemo@gmail.com
+
+class Vehiculo:
+    color: str = "Negro"
+    ruedas: int = 4
+    puertas: int = 5
+
+class coche (Vehiculo):
+    velocidad: int = 100
+    cilindrada: int = 1300
+
+miCarro = coche()
+print("El color de mi carro es: ", miCarro.color)
+print("Mi carro tiene:", str(miCarro.ruedas) +" ruedas")
+print("Mi carro tiene:", str(miCarro.puertas) +" ruedas")
+print("Mi carro tiene una velocidad maxima de: ", str(miCarro.velocidad))
+print("Mi carro tiene una cilindraje de:", str(miCarro.cilindrada))
